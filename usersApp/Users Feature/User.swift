fcb4eb628c3068f7a20383cae00ca5cdbefcba58
SantiagoCalvo <s.calvo11@uniandes.edu.co>
2023-03-28 19:46:17 -0500

    initial commit

---
+++ usersApp/Users Feature/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  usersApp
+//
+//  Created by santiago calvo on 28/03/23.
+//
+
+import Foundation
+
+struct User {
+    let id: UUID
+    let name: String
+    let phone: String
+}
