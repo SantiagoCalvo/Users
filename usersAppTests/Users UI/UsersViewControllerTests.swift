6f60b781d2ae1f4b708a79d761cd01d9fe3578a4
SantiagoCalvo <s.calvo11@uniandes.edu.co>
2023-03-29 20:04:58 -0500

    viewdidLoad load users

--- usersAppTests/Users UI/UsersViewControllerTests.swift
+++ usersAppTests/Users UI/UsersViewControllerTests.swift
@@ -20,6 +20,16 @@ class UsersViewController: UIViewController {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        load()
+    }
+    
+    private func load() {
+        loader.load { _ in }
+    }
 }
 
 class UsersViewControllerTests: XCTestCase {
@@ -30,6 +40,16 @@ class UsersViewControllerTests: XCTestCase {
         _ = UsersViewController(loader: loader)
         
         XCTAssertEqual(loader.loadCallCount, 0)
+    }
+    
+    func test_viewDidLoad_LoadsUsers() {
+        let loader = LoaderSpy()
+        
+        let sut = UsersViewController(loader: loader)
+        
+        sut.loadViewIfNeeded()
+        
+        XCTAssertEqual(loader.loadCallCount, 1)
     }
     
     //MARK: - helpers
